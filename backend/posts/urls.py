f034ea25242cc42f6fa91e4c78993e12c150b29c
Bharatk003 <bharatkshirsagar003@gmail.com>
2024-06-04 10:00:31 +0530

    add documentation and Schema

--- backend/posts/urls.py
+++ backend/posts/urls.py
@@ -1,10 +1,16 @@
 from django.urls import path
 from .views import PostList, PostDetail, UserList, UserDetail
+from rest_framework.schemas import get_schema_view # new
 
 urlpatterns = [
     path('users/', UserList.as_view()), # new
     path('users/<int:pk>/', UserDetail.as_view()),
     path('<int:pk>/', PostDetail.as_view()),
     path('', PostList.as_view()),
+    path('openapi', get_schema_view( # new
+        title="Blog API",
+        description="A sample API for learning DRF",
+        version="1.0.0"
+), name='openapi-schema'),
     
 ]
